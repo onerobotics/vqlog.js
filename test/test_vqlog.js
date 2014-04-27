f24d5ff0ee925a89523b7d72ecd6eaf5e2cc2d98
Jay Strybis <jay.strybis@gmail.com>
2014-04-27 13:19:48 -0500

    refactor how vqlog classes are available to main package

--- test/test_vqlog.js
+++ test/test_vqlog.js
@@ -1,4 +1,21 @@
-var Helper = require('./helper');
+var Helper = require('./helper'),
+    vqlog = require('../lib/vqlog'),
+    assert = require('assert');
 
 describe('vqlog', function() {
+  it('gives access to Entry', function() {
+    assert.equal("Entry", vqlog.Entry.name);
+  });
+
+  it('gives access to Event', function() {
+    assert.equal('Event', vqlog.Event.name);
+  });
+
+  it('gives access to Log', function() {
+    assert.equal('Log', vqlog.Log.name);
+  });
+
+  it('gives access to Utilities', function() {
+    assert.equal('Utilities', vqlog.Utilities.name);
+  });
 });
